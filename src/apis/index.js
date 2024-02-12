36b36af1f6a350ac7d33be87aeed6379a62ac231
ChavinduShirantha <chavindushirantha@gmail.com>
2024-02-12 17:47:03 +0530

    Updated Content

--- src/apis/index.js
+++ src/apis/index.js
@@ -8,4 +8,6 @@ export const getUserByIdAPI=async (id)=> axios.get(`/users/${id}`);
 
 export const createUserAPI=async (user)=> axios.post(`/users`,user);
 
-export const updateUserAPI=async (user)=> axios.post(`/users/${user.id}`,user);
+export const updateUserAPI=async (user)=> axios.put(`/users/${user.id}`,user);
+
+export const deleteUserByIdAPI=async (id)=> axios.delete(`/users/${id}`);
