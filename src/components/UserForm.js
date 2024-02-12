94bde273df38f11aff144dcce98b978f862ed639
ChavinduShirantha <chavindushirantha@gmail.com>
2024-02-12 18:17:37 +0530

    Updated Content

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,17 @@
+import {Button, Container, Input} from "@mui/material";
+
 const UserForm = () => {
-  return<h1>User Registration Form</h1>
+    return (
+        <>
+            <Container>
+                <Input value="id" fullWidth disabled />
+                <Input placeholder="Enter Name" value="name" fullWidth />
+                <Input placeholder="Enter Email" value="email" fullWidth />
+                <Input placeholder="Enter Password" value="password" fullWidth />
+                <Button fullWidth variant="contained">Submit</Button>
+            </Container>
+        </>
+    );
 }
 
 export default UserForm;
