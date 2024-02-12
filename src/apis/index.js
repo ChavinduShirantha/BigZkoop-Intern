d0480faa03dbf31b47a724807512288c92c9a8c4
ChavinduShirantha <chavindushirantha@gmail.com>
2024-02-12 17:43:25 +0530

    Updated Content

--- src/apis/index.js
+++ src/apis/index.js
@@ -5,3 +5,5 @@ axios.defaults.baseURL='http://localhost:8000';
 export const getUsersAPI=async ()=> axios.get('/users');
 
 export const getUserByIdAPI=async (id)=> axios.get(`/users/${id}`);
+
+export const createUserAPI=async (user)=> axios.post(`/users`,user);
