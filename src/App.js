383a2dab17727eb968033fb80fbf6791f89f9ffc
ChavinduShirantha <chavindushirantha@gmail.com>
2024-02-12 18:08:43 +0530

    Updated Content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from "@mui/material/Grid";
+import UserForm from "./components/UserForm";
+import UserTable from "./components/UserTable";
 
 function App() {
-  return (
-    <h1>
-      Hello App
-    </h1>
-  );
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4} lg={6}>
+                    <UserForm/>
+                </Grid>
+                <Grid item xs={12} md={8} lg={6}>
+                    <UserTable/>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default App;
