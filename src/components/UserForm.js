ed5d65d41a506ac64f6c84305217806d04ef4040
ChavinduShirantha <chavindushirantha@gmail.com>
2024-02-13 09:41:04 +0530

    Updated Content

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,13 +1,26 @@
 import {Button, Container, Input} from "@mui/material";
+import {useState} from "react";
 
 const UserForm = () => {
+    const [user, setUser] = useState({
+        id: 0,
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (prop) => (event) => {
+        setUser({...user, [prop]: event.target.value})
+    }
+
     return (
         <>
             <Container>
-                <Input value="id" fullWidth disabled />
-                <Input placeholder="Enter Name" value="name" fullWidth />
-                <Input placeholder="Enter Email" value="email" fullWidth />
-                <Input placeholder="Enter Password" value="password" fullWidth />
+                <Input value={user.id} fullWidth disabled/>
+                <Input onChange={handleChange('name')} placeholder="Enter Name" value={user.name} fullWidth/>
+                <Input onChange={handleChange('email')} placeholder="Enter Email" value={user.email} fullWidth/>
+                <Input onChange={handleChange('password')} placeholder="Enter Password" value={user.password}
+                       fullWidth/>
                 <Button fullWidth variant="contained">Submit</Button>
             </Container>
         </>
